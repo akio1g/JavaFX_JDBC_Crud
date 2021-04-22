949d84d97694176332d5c3fe1dca10002baae3e0
Gabriel Akio <akioumebayashi@gmail.com>
2021-04-22 20:12:52 -0300

    mudança no loadview

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,17 +1,26 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
+import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+	
+	private DepartmentService service;	
+	
 	@FXML
 	private TableView<Department> tvDepartment;
 	@FXML
@@ -22,17 +31,34 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 	
+	private ObservableList<Department> obsList;
+	
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("teste button");
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		initializeNodes();
 	}
 	private void initializeNodes() {
-		tcId.setCellValueFactory(new PropertyValueFactory<>("Id"));
+		tcId.setCellValueFactory(new PropertyValueFactory<>("Id")); // define nome da coluna
 		tcName.setCellValueFactory(new PropertyValueFactory<>("Name"));
+		
+		Stage stage = (Stage) Main.GetMainScene().getWindow();
+		tvDepartment.prefHeightProperty().bind(stage.heightProperty()); // macete pra tabela acompanhar a janela
 	}
-
+	
+	public void updateTableView() {
+		if(service == null) {
+			throw new IllegalStateException("Service was null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		tvDepartment.setItems(obsList);
+	}
 }
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -15,6 +16,7 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.services.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -32,19 +34,22 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMiDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
+		});
 	}
 
 	@FXML
 	public void onMiAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml", x -> {});
 	}
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 	}
 
-	private synchronized void loadView(String AbsoluteName) {
+	private synchronized <T> void loadView(String AbsoluteName, Consumer<T> initializingAction) {
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(AbsoluteName));
 			VBox newVBox = loader.load();
@@ -55,9 +60,13 @@ public class MainViewController implements Initializable {
 			mainVBox.getChildren().clear();
 			mainVBox.getChildren().add(mainMenu);
 			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			T controller = loader.getController();
+			initializingAction.accept(controller);
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
+	
 
 }
