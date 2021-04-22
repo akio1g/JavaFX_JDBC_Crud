0d9dbe5d2a412c679632f63f716c050fca5884c3
Gabriel Akio <akioumebayashi@gmail.com>
2021-04-22 03:23:44 -0300

    Projeto criado

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module projectJfxJDBC {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.fxml;
+	
+	opens application to javafx.graphics, javafx.fxml;
+	
+	opens gui to javafx.graphics, javafx.fxml;
+}
